d05db3a4be1e85c026e5195f22b1d49566e0a9ae
faellfaell <faellpapum@gmail.com>
2022-07-14 12:25:51 +0000

    algoritimo de busca modificado

--- projetos/algoritimos/c/search-algorithm.c
+++ projetos/algoritimos/c/search-algorithm.c
@@ -3,13 +3,13 @@
 
 int main(){
 
-  int lista[]={3,10,8,7,5,4,9,1,2,6};
-  int numero;
+  int arr[]={3,10,8,7,5,4,9,1,2,6};
+  int number;
   int i = 0;
 
   printf("digite um numero de 1 a 10\n");
-  scanf("%i", &numero);
-  while (numero != lista[i]){    
+  scanf("%i", &number);
+  while (number != arr[i]){    
     if(i == 9){
     printf("o numero digitado nao pertence a lista\n");
       return 1;
@@ -17,7 +17,7 @@ int main(){
    i++; 
   };
   
-  while (nummero != lista[i]) {
+  while (number != arr[i]) {
     i++;
   }
   printf("\n%i pertence a lista\n", arr[i]);
